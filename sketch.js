5d36ef85259cfccc5d483b7462a2571e699c0345
baolong2005 <135151094+baolong2005@users.noreply.github.com>
2024-05-25 20:20:29 +1200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,8 +3,8 @@ let maskImg=null;
 let renderCounter=0;
 
 // change these three lines as appropiate
-let sourceFile = "input_1.jpg";
-let maskFile   = "mask_1.png";
+let sourceFile = "input_3.jpg";
+let maskFile   = "mask_3.png";
 let outputFile = "output_1.png";
 
 function preload() {
@@ -18,29 +18,51 @@ function setup () {
 
   imageMode(CENTER);
   noStroke();
-  background(255, 0, 0);
+  background(0, 0, 0);
   sourceImg.loadPixels();
   maskImg.loadPixels();
 }
 
 function draw () {
-  for(let i=0;i<4000;i++) {
-    let x = floor(random(sourceImg.width));
-    let y = floor(random(sourceImg.height));
-    let pix = sourceImg.get(x, y);
-    let mask = maskImg.get(x, y);
-    fill(pix);
+  let j = renderCounter;
+  // get one scanline
+  for(let i=0; i<1920; i++) {
+    let pix = sourceImg.get(i, j);
+    let mask = maskImg.get(i, j);
     if(mask[0] > 128) {
-      let pointSize = 10;
-      ellipse(x, y, pointSize, pointSize);
+      // draw the full pixels
+      set(i, j, pix);
     }
     else {
-      let pointSize = 20;
-      rect(x, y, pointSize, pointSize);    
+      // draw a "dimmed" version in gray
+      let gray_color = 64 + pix[1] / 8;
+      set(i, j, gray_color);
     }
   }
+  // for(let i=0;i<4000;i++) {
+  //   let x = floor(random(sourceImg.width));
+  //   let y = floor(random(sourceImg.height));
+  //   let pix = sourceImg.get(x, y);
+  //   let mask = maskImg.get(x, y);
+  //   fill(pix);
+  //   if(mask[0] > 128) {
+
+  //     stroke(pix);
+  //     strokeWeight(height/500);
+  //     console.log(pix[0])
+  //     if(pix[0] > 100){
+  //       line(x, y, x + width/100, y)
+  //       line(x + 100, y + 100, x + width/100 + 100, y + 100)
+  //     } else{
+  //       line(x, y, x - width/100, y)
+  //       line(x + 100, y + 100, x - width/100 + 100, y + 100)
+  //     }
+  //   }
+
+  // }
+  updatePixels();
   renderCounter = renderCounter + 1;
-  if(renderCounter > 10) {
+  if(renderCounter > 1080) {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
