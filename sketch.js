0b764d38268d9f6896f056be6697a5885681a6ae
baolong2005 <135151094+baolong2005@users.noreply.github.com>
2024-05-31 23:02:06 +1200

    final

--- sketch.js
+++ sketch.js
@@ -3,9 +3,9 @@ let maskImg=null;
 let renderCounter=0;
 
 // change these three lines as appropiate
-let sourceFile = "input_1.jpg";
-let maskFile   = "mask_1.png";
-let outputFile = "output_1.png";
+let sourceFile = "input_3.jpg";
+let maskFile   = "mask_3.png";
+let outputFile = "output_3.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -24,21 +24,7 @@ function setup () {
 }
 
 function draw () {
-  // let j = renderCounter;
-  // // get one scanline
-  // for(let i=0; i<1920; i++) {
-  //   let pix = sourceImg.get(i, j);
-  //   let mask = maskImg.get(i, j);
-  //   if(mask[0] > 128) {
-  //     // draw the full pixels
-  //     set(i, j, pix);
-  //   }
-  //   else {
-  //     // draw a "dimmed" version in gray
-  //     let gray_color = 64 + pix[1] / 8;
-  //     set(i, j, gray_color);
-  //   }
-  // }
+
   for(let i=0;i<4000;i++) {
     let x = floor(random(sourceImg.width));
     let y = floor(random(sourceImg.height));
@@ -50,22 +36,23 @@ function draw () {
       stroke(pix);
       strokeWeight(height/500);
       console.log(mask[0])
-      if(pix[0] > 100){
-        line(x, y, x + width/100, y)
-        // line(x + 100, y + 100, x + width/100 + 100, y + 100)
+      if(pix[0] > 100){ 
+        line(x, y, x - width/1000, y - height/1000);
+         
       } else{
-        line(x, y, x - width/100, y)
-        // line(x + 100, y + 100, x - width/100 + 100, y + 100)
+        line(x, y, x - width/1000, y - height/1000);
+         
       }
      } else{ 
       noStroke();
-      rect(x, y, 5 , 5);
+      rect(x, y, 15 , 15);
+      
     }
 
   }
-  // updatePixels();
+ 
   renderCounter = renderCounter +1 
-  if(renderCounter > 100) {
+  if(renderCounter > 1080) {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
